d0e820ecdc7407b005be9eb65b7bf13ae0f19c09
Artanyan <71780084+Artanyan@users.noreply.github.com>
2021-05-15 07:14:50 +0300

    DRY

--- src/theme-switcher.js
+++ src/theme-switcher.js
@@ -1,46 +1,35 @@
-const themeSwitcherEl = document.querySelector('#theme-switch-toggle');
-const bodyEl = document.querySelector('body');
-
-const Theme = {
-  LIGHT: 'light-theme',
-  DARK: 'dark-theme',
-};
-
-// LOCAL STORAGE CURRENT THEME
-
-if (!localStorage.getItem('Theme')) {
-    bodyEl.classList.add('light-theme');
-    localStorage.setItem('Theme', Theme.LIGHT);
-};
-
-if (localStorage.getItem('Theme') === Theme.DARK) {
-    bodyEl.classList.add('dark-theme');
-    bodyEl.classList.remove('light-theme');
-    themeSwitcherEl.checked = true;
-};
-
-if (localStorage.getItem('Theme') === Theme.LIGHT) {
-    bodyEl.classList.add('light-theme');
-    bodyEl.classList.remove('dark-theme');
-    themeSwitcherEl.checked = false;
-};
-
-// THEME SWITCHER
-
-themeSwitcherEl.addEventListener('change', themeSwitcher);
-
-function themeSwitcher() {
-    if (themeSwitcherEl.checked) {
-        bodyEl.classList.add('dark-theme');
-        bodyEl.classList.remove('light-theme');
-        localStorage.setItem('Theme', Theme.DARK);
-        return
-    };
-
-    if (!themeSwitcherEl.checked) {
-        bodyEl.classList.add('light-theme');
-        bodyEl.classList.remove('dark-theme');
-        localStorage.setItem('Theme', Theme.LIGHT);
-    };
-};
-
+  const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
+  const THEME_KEY = 'theme';
+  const themeSwitcher = document.querySelector('#theme-switch-toggle');
+  
+  setTheme(Theme);
+  themeSwitcher.addEventListener('change', switchTheme);
+  
+  function setTheme({LIGHT, DARK}) {
+      if (localStorage.getItem(THEME_KEY)===null) {
+          document.body.classList.add(LIGHT);
+          localStorage.setItem(THEME_KEY, LIGHT)
+      } else {
+          document.body.className = ' ';
+          document.body.classList.add(`${localStorage.getItem(THEME_KEY)}`);
+              if (localStorage.getItem(THEME_KEY) === DARK) {
+                  themeSwitcher.setAttribute('checked', 'true')
+          };
+  
+      };
+      
+  };
+  
+  function switchTheme() {
+      if (localStorage.getItem(THEME_KEY) === Theme.LIGHT) {
+          localStorage.setItem(THEME_KEY, Theme.DARK);
+          setTheme(Theme);
+      } else if (localStorage.getItem(THEME_KEY) === Theme.DARK) {
+          localStorage.setItem(THEME_KEY, Theme.LIGHT);
+          setTheme(Theme);
+      };
+  
+  };
